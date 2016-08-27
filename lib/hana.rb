66a80fa99c0f1cc4ff4ee683977706adc5af9c6a
temifowotade <temitope.fowotade@andela.com>
2016-08-27 18:05:00 +0100

    chore: adds some utility methods like snake_case and camel_case

--- lib/hana.rb
+++ lib/hana.rb
@@ -1,5 +1,6 @@
 require "hana/version"
-require "hana/controller.rb"
+require "hana/controller.rb" 
+require "hana/utils.rb" 
 
 module Hana
   class Application
@@ -21,7 +22,7 @@ module Hana
 
     def get_controller_and_action(env)
       _, controller_name, action = env["PATH_INFO"].split("/")
-      controller_name = controller_name.capitalize + "Controller"
+      controller_name = controller_name.to_camel_case + "Controller"
       [ Object.const_get(controller_name), action ]
     end
   end
