c4068d03117bee90dee64870bbd28a04b1e48bfa
temifowotade <temitope.fowotade@andela.com>
2016-09-01 15:05:22 +0100

    feat: Implements view rendering using erubis and routing with controllers and actions only. [finishes #129197687]

--- lib/hana.rb
+++ lib/hana.rb
@@ -2,29 +2,15 @@ require "hana/version"
 require "hana/controller.rb" 
 require "hana/utils.rb"
 require "hana/dependencies.rb"
+require "hana/routing.rb"
 
 module Hana
   class Application
     def call(env)
-      if env["PATH_INFO"] == "/"
-        return [ 302, { "Location" => "/todo/new" }, [] ]
-      end
-
       if env["PATH_INFO"] == "/favicon.ico"
         return [ 500, {}, [] ]
       end
-
-      # env["PATH_INFO"] = "/todo/new" => TodoController.send(:new)
-      controller_class, action = get_controller_and_action(env)
-      response = controller_class.new.send(action)
-
-      [ 200, { "Content-Type" => "text/html" }, [ response ] ]
-    end
-
-    def get_controller_and_action(env)
-      _, controller_name, action = env["PATH_INFO"].split("/")
-      controller_name = controller_name.to_camel_case + "Controller"
-      [ Object.const_get(controller_name), action ]
+      get_rack_app(env).call(env)
     end
   end
 end
