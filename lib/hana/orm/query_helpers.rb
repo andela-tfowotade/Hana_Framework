adbbe96c2b8b3ade755f4a2ce0cb738d24a414dd
temifowotade <temitope.fowotade@andela.com>
2016-09-06 02:50:32 +0100

    feat(orm): builds query helpers

--- lib/hana/orm/query_helpers.rb
+++ lib/hana/orm/query_helpers.rb
@@ -1,4 +1,61 @@
 module Hana
   module QueryHelpers
+    def create_table
+      Database.execute <<-SQL
+        CREATE TABLE IF NOT EXISTS "#{@table_name} ( #{@table_properties} )"
+      SQL
+    end
+
+    def to_table(table_name)
+      attr_accessor @table_name
+      @table_name = table_name
+    end
+
+    def property(column_name, column_attributes)
+      @properties ||= {}
+      @properties[column_name] = Openstruct.new(column_attributes)
+      attr_accessor column_name
+    end
+
+    def properties
+      @properties[:id] = Openstruct.new({ type: "integer", primary_key: true })
+      @properties[:created_at] = Openstruct.new({ type: "varchar(10)" })
+      @properties
+    end
+
+    def new_record_placeholder
+      (['?']) * properties.size).join(', ')
+    end
+
+    def update_record_placeholder
+      columns.map { |property| "#{property}=?" }.join(', ')
+    end
+
+    def query_string(column_attributes)
+      query_builder = []
+      query_builder << column_attributes.type
+      query_builder << "primary key autoincrement" if column_attributes.primary_key
+      query_builder << "not null" unless column_attributes.nullable || column_attributes.nullable.nil?
+      query_builder.join(" ")        
+    end
+
+    def initialize_query_holder
+      attr_accessor :id, :created_at
+      query_holder = []
+      query_holder
+    end
+
+    def table_properties
+      query_holder = initialize_query_holder
+
+      properties.each do |column_name, column_attributes|
+        query_holder << "#{column_name} #{ query_string(column_attributes) }"
+      end
+      query_holder.join(", ")
+    end
+
+    def columns
+      properties.keys
+    end
   end
 end
