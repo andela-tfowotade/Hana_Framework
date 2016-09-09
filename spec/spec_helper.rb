69f574596d317a709a1fe821d91bdd8011d43ea7
temifowotade <temitope.fowotade@andela.com>
2016-09-09 17:12:42 +0100

    fix: resolves travisci issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,12 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require "simplecov"
 SimpleCov.start "rails"
-require "coveralls"
-Coveralls.wear! "rails"
+# require "coveralls"
+# Coveralls.wear! "rails"
 require "todo/config/application"
 require 'rspec'
 require 'rack/test'
- 
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 
 ENV['RACK_ENV'] = 'test'
